d08c8344278fd851461877bb180822c9e48aeed1
Christoph Guttandin <chrisguttandin@media-codings.com>
2015-11-21 05:21:46 +0100

    move test data in common file

--- test/server/integration/module.js
+++ test/server/integration/module.js
@@ -1,79 +1,14 @@
 'use strict';
 
 var createReadStream = require('fs').createReadStream,
-    metadataDetector = require('../../../src/module.js');
+    metadataDetector = require('../../../src/module.js'),
+    testData = require('../../fixtures/test-data.json');
 
 describe('metadata-detector', function () {
 
     describe('createReadStream()', function () {
 
-        leche.withData([
-            [
-                '1000-frames-of-noise-encoded-and-tagged-with-itunes.mp3',
-                [
-                    [ 0, 2129 ]
-                ]
-            ], [
-                '1000-frames-of-noise-encoded-with-ffmpeg-and-tagged-with-vlc.flac',
-                [
-                    [ 0, 8376 ]
-                ]
-            ], [
-                '1000-frames-of-noise-encoded-with-ffmpeg-and-tagged-with-vlc.mp3',
-                [
-                    [ 0, 4928 ],
-                    [ 8061, 8189 ]
-                ]
-            ], [
-                '1000-frames-of-noise-encoded-with-ffmpeg.flac',
-                [
-                    [ 0, 8376 ]
-                ]
-            ], [
-                '1000-frames-of-noise-encoded-with-ffmpeg.mp3',
-                [
-                    [ 0, 3870 ]
-                ]
-            ], [
-                '1000-frames-of-noise-encoded-with-itunes-and-tagged-with-mp3tag.mp3',
-                [
-                    [ 0, 2129 ],
-                    [ 5264, 5392 ]
-                ]
-            ], [
-                '1000-frames-of-noise-encoded-with-itunes-and-tagged-with-vlc.mp3',
-                [
-                    [ 0, 2129 ],
-                    [ 5264, 5392 ]
-                ]
-            ], [
-                '1000-frames-of-noise-encoded-with-itunes.mp3',
-                [
-                    [ 0, 2129 ]
-                ]
-            ], [
-                '1000-frames-of-noise-encoded-with-soundbooth-and-tagged-with-vlc.mp3',
-                [
-                    [ 0, 6471 ],
-                    [ 8559, 8687 ]
-                ]
-            ], [
-                '1000-frames-of-noise-encoded-with-soundbooth.mp3',
-                [
-                    [ 0, 5370 ]
-                ]
-            ], [
-                '1000-frames-of-noise-encoded-with-toast-and-tagged-with-vlc.flac',
-                [
-                    [ 0, 4233 ]
-                ]
-            ], [
-                '1000-frames-of-noise-encoded-with-toast.flac',
-                [
-                    [ 0, 4233 ]
-                ]
-            ]
-        ], function (filename, locations) {
+        leche.withData(testData, function (filename, locations) {
 
             it('should strip the id3 tag from the file', function (done) {
                 var lctns = [],
