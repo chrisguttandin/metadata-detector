d851d4a2bd39f56c4a434349766b93f6b308797b
chrisguttandin <chrisguttandin@media-codings.com>
2016-08-12 22:44:14 +0200

    disable Chrome Canary tests on SauceLabs

--- config/karma/unit.js
+++ config/karma/unit.js
@@ -43,7 +43,7 @@ module.exports = function (config) {
 
     if (process.env.TRAVIS) {
         configuration.browsers = [
-            'ChromeCanarySauceLabs',
+            // 'ChromeCanarySauceLabs',
             'ChromeSauceLabs',
             // 'FirefoxDeveloperSauceLabs',
             'FirefoxSauceLabs',
