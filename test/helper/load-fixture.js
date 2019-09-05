084ba17635e3f16cedfcf6bb6ddc57b74cac060d
Christoph Guttandin <chrisguttandin@media-codings.com>
2019-09-05 12:55:54 +0200

    test: remove base64 workaround

--- test/helper/load-fixture.js
+++ test/helper/load-fixture.js
@@ -1,14 +1,3 @@
-const base64ToArrayBuffer = (encodedData) => {
-    const decodedData = atob(encodedData.replace(/\s/g, ''));
-    const uint8Array = new Uint8Array(decodedData.length);
-
-    Array.prototype.forEach.call(uint8Array, (value, index) => {
-        uint8Array[index] = decodedData.charCodeAt(index);
-    });
-
-    return uint8Array.buffer;
-};
-
 export const loadFixtureAsArrayBuffer = (fixture, callback) => {
     const request = new XMLHttpRequest();
 
@@ -16,17 +5,10 @@ export const loadFixtureAsArrayBuffer = (fixture, callback) => {
         callback('request-failed');
     };
     request.onload = function (event) {
-        if (fixture.slice(-4) === '.txt') {
-            callback(null, base64ToArrayBuffer(event.target.response));
-        } else {
-            callback(null, event.target.response);
-        }
+        callback(null, event.target.response);
     };
     request.open('GET', 'base/test/fixtures/' + fixture);
-
-    if (fixture.slice(-4) !== '.txt') {
-        request.responseType = 'arraybuffer';
-    }
+    request.responseType = 'arraybuffer';
 
     request.send();
 };
