d58cd2179e19a08434f785e87d92f9063cb8526d
Christoph Guttandin <chrisguttandin@media-codings.com>
2025-12-15 01:42:44 +0100

    build(config): run tests with Vitest

--- test/integration/module.js
+++ test/integration/module.js
@@ -1,4 +1,5 @@
-import * as metadataDetector from '../../src/module';
+import { beforeEach, describe, expect, it } from 'vitest';
+import { locate, strip } from '../../src/module';
 import lengthsData from '../fixtures/lengths-data.json';
 import { loadFixtureAsArrayBuffer } from '../helper/load-fixture';
 import locationsData from '../fixtures/locations-data.json';
@@ -13,7 +14,7 @@ describe('metadata-detector', () => {
             });
 
             it('should locate the metadata tags of the file', async () => {
-                const detectedLocations = await metadataDetector.locate(arrayBuffer);
+                const detectedLocations = await locate(arrayBuffer);
 
                 expect(detectedLocations).to.deep.equal(locations);
             });
@@ -29,7 +30,7 @@ describe('metadata-detector', () => {
             });
 
             it('should strip the metadata tags from the file', async () => {
-                const strippedArrayBuffer = await metadataDetector.strip(arrayBuffer);
+                const strippedArrayBuffer = await strip(arrayBuffer);
 
                 expect(strippedArrayBuffer.byteLength).to.equal(byteLength);
             });
